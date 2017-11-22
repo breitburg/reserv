590a711b2d57735d745254cffde2d1c57cb4aeef
ilya400014 <ilya.batuxtin@gmail.com>
2017-11-22 20:28:09 +0300

    Добавлены какие-то изменения

--- Reserv.py
+++ Reserv.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
 root = Tk()
+root.title("Reserv")
+root.geometry("650x400+0+0")
+root.resizable(False, False)
+root.icon("resources/icon.png")
 
-
-
-root.mainloop
+root.mainloop()
