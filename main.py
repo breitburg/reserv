8c7940b3a56b85c0553943eee270326a2050fa0d
Ketsu8 <ketsoo@inbox.ru>
2017-11-24 18:42:33 +0300

    Изменения

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Reserv - главный скрипт
+
+Copyright © Upbits Team, 2017 – 2018.
+GitHub: http://github.com/Upbits/
+
+По вопросом Reserv писать сюда:
+https://vk.com/reserv или на почту
+<reserv@blinkhub.ru>.
+
+Удачного использования :D
+'''
+import os
+import wget
+import zipfile
+import gui
+
+
---
+++ testmail.py
@@ -0,0 +1,2 @@
+import main
+dada = main()
