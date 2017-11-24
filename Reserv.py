6b2c6348a745ba31c9e840df640d345c21940978
ilya400014 <ilya.batuxtin@gmail.com>
2017-11-24 17:52:11 +0300

    Переделан код

--- Reserv.py
+++ Reserv.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 
 root = Tk()
@@ -11,7 +10,7 @@ label_welcome = Label(root,text="Добро пожаловать в Reserv")
 label_welcome.place(x=225,y=350,width=200,height=350)
 
 label_copyright = Label(root,text="Copyright © 2017")
-label_copyright.place(x=225,y=150,width=200,height=350)
+label_copyright.place(x=225,y=390,width=200,height=350)
 
 button_1 = Button(root,text="Начать",bg="white",fg="black",font="arial")
 button_1.place(x=275,y=190,width=100,height=35)
