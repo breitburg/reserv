5a2b4230d7937a2e094c6a0788fef04c491a27b6
ilya400014 <ilya.batuxtin@gmail.com>
2017-11-24 17:54:47 +0300

    Маленькие изменения

--- Reserv.py
+++ Reserv.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 
 root = Tk()
@@ -6,11 +7,11 @@ root.geometry("650x400+0+0")
 root.resizable(False, False)
 root.iconbitmap("resources/icon.ico")
 
-label_welcome = Label(root,text="Добро пожаловать в Reserv")
-label_welcome.place(x=225,y=350,width=200,height=350)
+label_welcome = Label(root,text="Добро пожаловать в Reserv",font="arial")
+label_welcome.place(x=200,y=150,width=250,height=50)
 
-label_copyright = Label(root,text="Copyright © 2017")
-label_copyright.place(x=225,y=390,width=200,height=350)
+label_copyright = Label(root,text="Copyright © 2017",font="arial")
+label_copyright.place(x=200,y=350,width=250,height=50)
 
 button_1 = Button(root,text="Начать",bg="white",fg="black",font="arial")
 button_1.place(x=275,y=190,width=100,height=35)
