7b28840bae1ba733437089bca55b9c4fa451dd03
Breitburg <ketsoo@inbox.ru>
2017-12-02 00:04:45 +0300

    Ultimate update

--- main.py
+++ main.py
@@ -16,92 +16,59 @@ import zipfile
 import wget
 import essintials
 
-serverName = ""
-serverMode = ""
-serverPort = ""
-serverRcon = ""
-global serverName
-global serverMode
-global serverRcon
-global serverPort
-
-def basicConfigurate():
-    essintials.universalClear()
-    serverName = input("- Мастер конфигурации Reserv -\nДля дальнейшей сборки сервера просим вас пройти первоначальную настройку всех параметров по которым в дальшейшем будет собран ваш сервер.\n\nВведите название сервера: ")
-    serverPort = input("Какой порт вы желаете использовать? (по-умолчанию 25565) ")
-    if serverPort == "":
-        print("Устанавливаем порт 25565. Как по-умолчанию.")
-        serverPort = "25565"
-
-    serverMode = input("Допускать не лицензированные копии игры на сервер? (да/нет) ")
-    if serverMode != "да" and serverMode != "нет":
-        print("Эм.. " + serverMode + "? Что? Не понял.. Короче, сделаем так чтоб всех пускало, а то ты какой-то непонятный.")
-        serverMode = "нет"
-
-    serverRcon = input("Хотите-ли использовать протокол удаленного управления сервером RCON? (да/нет) ")
-    if serverRcon == "да":
-        rconPort = input("Ох, ну раз так введите желаемый порт для RCON: ")
-        rconPassword = input("Осталось только ввести пароль (только англ.): ")
-        print("Протокол RCON успешно конфигурирован.")
-    elif serverRcon != "нет":
-        print("Эм.. " + serverRcon + "? Что? Короче нахрен вырубим лучше этот RCON, а то еще взломает кто.")
-        serverRcon = "нет"
-    if input("\nКонфигурация сервера успешно проведена. Давайте проверим, всем ли вы удовлетворены.\nИмя: " + serverName + "\nПорт: " + serverPort + "\nОнлайн-режим: " + serverMode + "\nRcon: " + serverRcon + "\nВсе нормально? (да/нет) ") == "да":
-        if serverMode == "да":
-            serverMode = "true"
-        else:
-            serverMode = "false"
-        if serverRcon == "да":
-            serverRcon = "true"
-        else:
-            serverRcon = "false"
-
-        advncConfigurate()
-    else:
-        basicConfigurate()
-
-def advncConfigurate():
-    essintials.universalClear()
-    serverCore = input("- Конфигурация ядра Reserv - \n\nВыберите версию ядра из предложенных (1.12, 1.11, 1.10, 1.9, 1.8): ")
-    if serverCore == "1.12" or serverCore == "1.11" or serverCore == "1.10" or serverCore == "1.9" or serverCore == "1.8":
-        print("Установлена версия ядра " + serverCore + ".")
-    else:
-        print("Какая-то странная версия у тебя. " + serverCore + ". Кажись такой у нас нет. Поставим тебе последнюю.")
-        serverCore = "1.12"
-    essintials.universalClear()
-    if input("Конфигурация была завершена. Начать сборку сервера? (да/нет)") == "да":
-        essintials.universalClear()
-        print("Загрузка сервера...")
-        wget.download("http://hack.blinkhub.ru/reserv/server.zip")
-        print("Распаковка сервера...")
-        essintials.unZip("server.zip", "server/")
-        os.remove("server.zip")
-        print("Загрузка ядра....")
-        wget.download("http://hack.blinkhub.ru/reserv/cores/" + serverCore + ".jar", "server/")
-        print("Настройка параметров...")
-        servProperties = essintials.textFromFile("server/server.properties")
-        global serverName
-        global serverMode
-        global serverRcon
-        global serverPort
-        essintials.textToFile(servProperties.replace("server-port=RESERV", "server-port=" + serverPort).replace("online-mode=RESERV", "online-mode=" + serverMode).replace("motd=RESERV", "motd=" + serverName).replace("enable-rcon=RESERV", "enable-rcon=" + serverRcon), "server/server.properties")
-        print("Сборка сервера завершена.")
-
-
-
-#тут начало исполняемого кода
-
 essintials.universalClear()
-userChoice = input("Reserv " + essintials.colorText("(первая сборка)", "red") + "\nCopyright © Ketsu8, All rights reserved\nGithub: http://github.com/Upbits/Reserv\n\n(Z) Нормальный запуск. Так-как будет у обычных людей.\n(A) Базовая конфигурация\n(B) Продвинутая конфигурация\n(C) Сборка сервера\n")
-if userChoice == "Z" or userChoice == "z":
-    if input("- Лицензионное соглашение Mojang -\nЛицензионное соглашение: https://account.mojang.com/terms\n\nПомните, что все права на исходные коды серверов и плагинов пренадлежат из владельцам. Мы никоем оброзам не пытаемся присвоить из себе.\n\nЯ согласен(а)? (да/нет) ") == "да":
-        basicConfigurate()
+input("Добро пожаловать в Reserv\nCopyright © Ketsu8, All rights reserved.\nGithub: http://github.com/Upbits/Reserv\n\nНажмите на (Enter) чтобы приступить к сборке собственного сервера Minecraft...\n")
+essintials.universalClear()
+input("Лицензионное соглашение Reserv\nCopyright © Ketsu8, All rights reserved.\n\nВсе права на исходный код серверов\nи плагинов пренадлежат их праваобладателям.\nМы никоим образом не пытаемся присвоить их себе. В то\nвремя как права на исходный код Reserv пренадлежат\nкоманде UpBits на момент 2017 - 2018г.\n\nЕсли вы согласны, то нажмите на (Enter).\n")
+essintials.universalClear()
+serverName = input("Сборка сервера Reserv\nCopyright © Ketsu8, All rights reserved.\n\nВведите название сервера: ")
+serverPort = input("Какой порт вы желаете использовать? (по-умолчанию 25565) ")
+if serverPort == "":
+    print("Применена настройка по-умолчанию.")
+    serverPort = "25565"
+
+serverMode = input("Допускать не лицензированные копии игры на сервер? (да/нет) ")
+if serverMode != "да" and serverMode != "нет":
+    print("Применена настройка по-умолчанию.")
+    serverMode = "нет"
+
+serverRcon = input("Хотите-ли использовать протокол удаленного управления сервером RCON? (да/нет) ")
+if serverRcon == "да":
+    rconPort = input("Ох, ну раз так введите желаемый порт для RCON: ")
+    rconPassword = input("Осталось только ввести пароль (только англ.): ")
+elif serverRcon != "нет":
+    print("Применена настройка по-умолчанию.")
+    serverRcon = "нет"
+serverCore = input("Выберите версию ядра (c 1.8 по 1.12): ")
+if serverCore == "1.12" or serverCore == "1.11" or serverCore == "1.10" or serverCore == "1.9" or serverCore == "1.8":
+    print("Установлена версия ядра " + serverCore + ".")
+else:
+    print("Применена настройка по-умолчанию.")
+    serverCore = "1.12"
+essintials.universalClear()
+if input("Reserv Builder\nCopyright © Ketsu8, All rights reserved\n\nСервер " + serverName + " был успешно сконфигурирован. Вы выбрали версию " + serverCore + ", онлайн-режим установили на " + serverMode + ", использовали порт " + serverPort + " и установили статус RCON на " + serverRcon + ".\n\n(Y) Запустить сборку\n(B) Отменить сборку\n") == "Y":
+    essintials.universalClear()
+    print("Загрузка сервера...")
+    wget.download("http://hack.blinkhub.ru/reserv/server.zip")
+    print("Распаковка сервера...")
+    essintials.unZip("server.zip", "server/")
+    os.remove("server.zip")
+    print("\nЗагрузка ядра....")
+    wget.download("http://hack.blinkhub.ru/reserv/cores/" + serverCore + ".jar", "server/")
+    print("\nНастройка параметров...")
+    servProperties = essintials.textFromFile("server/server.properties")
+
+    if serverMode == "да":
+        serverMode = "true"
     else:
-        print("Ну ладно. Тогда пока.")
-        quit(0)
-elif userChoice == "A" or userChoice == "a":
-    basicConfigurate()
-elif userChoice == "B" or userChoice == "b":
-    advncConfigurate()
-elif userChoice == "C" or userChoice == "c":
-    print("Сборки пока нет. хдхдхдхдхдхдхдхд завтра ее сделаю")
+        serverMode = "false"
+    if serverRcon == "да":
+        serverRcon = "true"
+    else:
+        serverRcon = "false"
+
+    essintials.textToFile("server/server.properties", servProperties.replace("server-port=RESERV", "server-port=" + serverPort).replace("online-mode=RESERV", "online-mode=" + serverMode).replace("motd=RESERV", "motd=" + serverName).replace("enable-rcon=RESERV", "enable-rcon=" + serverRcon))
+    print("\nСборка сервера завершена.")
+else:
+    print("Отмена сборки сервера.")
+    quit(5)
