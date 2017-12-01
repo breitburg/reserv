9643227be0432eb012f837326a4a555dcf57555a
ilya400014 <ilya.batuxtin@gmail.com>
2017-12-01 21:47:40 +0300

    Комментарный коммит

---
+++ test.py
@@ -0,0 +1,12 @@
+import gui
+
+objWelcomeWindow = gui.showWelcomeWindow()
+objWelcomeWindow.show()
+
+objPathWindow = gui.showPathWindow()
+objPathWindow.show()
+
+VersionArray = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4]
+
+objVersionWindow = gui.showVersionWindow()
+print(objVersionWindow.show(VersionArray))
