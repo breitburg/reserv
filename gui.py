9b6c10439395f3d9d16ac9cc3c139dbfa1b2f976
Ketsu8 <ketsoo@inbox.ru>
2017-12-30 23:52:24 +0300

    Big update

--- gui.py
+++ gui.py
@@ -4,10 +4,13 @@
 Copyright © Upbits Team, 2017.
 Требования: Любая система, Python 3, Tkinter library
 '''
+
 from tkinter import * #Импорт tkinter'а
 import tkinter.ttk as ttk #Импорт ttk
 from tkinter import filedialog #Импорт из tkinter'а filedialog
 from tkinter import messagebox #Импорт из tkinter'а messagebox
+from belfrywidgets import ToolTip #Импорт из belfrywidgets ToolTip
+import platform, threading #Импорт platform,threading
 
 class showWelcomeWindow():
     def show(self):
@@ -17,7 +20,7 @@ class showWelcomeWindow():
         self.root.title("Reserv") #Сэтим имя окна
         self.root.geometry("650x400+" + str(windowX) + "+" + str(windowY)) #Сэтим размер окна
         self.root.resizable(False, False) #Установка возможности изменить размер окна на False (По ширине и длине)
-        self.root.protocol("WM_DELETE_WINDOW", quit) #Установка зарезервированной функции quit на кнопку "X" 
+        self.root.protocol("WM_DELETE_WINDOW", quit) #Установка зарезервированной функции quit на кнопку "X"
 
         s = ttk.Style()
         s.configure("TButton",bg="white",fg="black",font="arial 10") #Создание стиля "TButton"
@@ -32,61 +35,14 @@ class showWelcomeWindow():
         nextButton.place(x=275,y=190,width=100,height=28) #Размещение кнопки по кординатам
 
         self.root.mainloop()
-        pass
 
     def destroyWindow(self):
         self.root.destroy() #Уничтожение окна
         pass
     pass
 
-class showPathWindow():
-    def show(self):
-        self.root = Tk()
-        windowX = int(self.root.winfo_screenwidth() / 2 - 325) #Вычисление центра экрана пользователя по X'у
-        windowY = int(self.root.winfo_screenheight() / 2 - 200) #Вычисление центра экрана пользователя по Y'у
-        self.root.title("Reserv") #Сэтим имя окна
-        self.root.geometry("650x400+" + str(windowX) + "+" + str(windowY)) #Сэтим размер окна
-        self.root.resizable(False, False) #Установка возможности изменить размер окна на False (По ширине и длине)
-        self.root.protocol("WM_DELETE_WINDOW", self.appExit) #Установка функции appExit на кнопку "X"
-
-        s = ttk.Style()
-        s.configure("TButton",font="arial 10") #Создание стиля "TButton"
-        s.configure("S.TButton",font="arial 7") #Создание стиля "S.TButton"
-        s.configure("TEntry",font="arial 10") #Создание стиля "TEntry"
-
-        informationLabel = Label(self.root,text="Выберите директорию сервера",font="arial 10") #Cоздание текста со свойствами
-        informationLabel.place(x=225,y=160,width=200,height=20) #Размещение текста по кординатам и установка площади
-
-        self.directoryEntry = ttk.Entry(self.root,style="TEntry") #Cоздание поля со свойствами
-        self.directoryEntry.place(x=225,y=200,width=200,height=20) #Размещение поля по кординатам и установка площади
-
-        dialogButton = ttk.Button(self.root,text="...",style="S.TButton",command=self.showFileDialog) #Создание кнопки со свойствами
-        dialogButton.place(x=425,y=200,width=20,height=20) #Размещение кнопки по кординатам и установка площади
-
-        nextButton = ttk.Button(self.root,text="далее",style="TButton",command=self.nextAction) #Создание кнопки со свойствами
-        nextButton.place(x=485,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
-
-        self.root.mainloop()
-        pass
-    
-    def showFileDialog(self):
-        self.root.filename = filedialog.askdirectory() #Создание диалога выбора папки и запись результата в переменную
-        self.directoryEntry.delete('0', END) #Удаление текста из directoryEntry
-        self.directoryEntry.insert(1, self.root.filename) #Добавление результата работы диалога папки в directoryEntry
-        pass
-
-    def nextAction(self):
-        self.root.destroy() #Уничтожение окна
-        pass
-    
-    def appExit(self):
-        if messagebox.askokcancel("Выйти?","Вы точно хотите выйти из программы?"): #Если результат ответа от пользователя положительный
-            quit() #то выполняется зарезервированная функция quit
-            pass
-        pass
-
-class showVersionWindow():
-    def show(self,VersionArray):
+class showSettingsWindow():
+    def show(self,name,port,onlineMode,CBE,PVP,D,GM):
         self.returnArray = [] #Создание массива returnArray
         self.root = Tk()
         windowX = int(self.root.winfo_screenwidth() / 2 - 325) #Вычисление центра экрана пользователя по X'у
@@ -97,7 +53,130 @@ class showVersionWindow():
         self.root.protocol("WM_DELETE_WINDOW", self.appExit) #Установка функции appExit на кнопку "X"
 
         s = ttk.Style()
-        s.configure("TButton",font="arial 10") #Создание стиля "TButton"
+        s.configure("TButton",bg="white",fg="black",font="arial 10") #Создание стиля "TButton"
+        s.configure("TEntry",font="arial 10") #Создание стиля "TEntry"
+
+        self.onlineMode_Choose = IntVar()
+        self.onlineMode_Choose.set(1)
+
+        self.CBE_Choose = IntVar()
+        self.CBE_Choose.set(0)
+
+        self.PVP_Choose = IntVar()
+        self.PVP_Choose.set(1)
+
+        name_Label = Label(self.root,text="Описание сервера:",font="arial 10")
+        name_Label.place(x=0,y=0)
+        ToolTip(name_Label,"Описание сервера, отображаемое при подключении в списке серверов. Поддерживает форматирование текста.")
+        self.name_Entry = ttk.Entry(self.root,style="TEntry")
+        self.name_Entry.place(x=120,y=0,width=190,height=20)
+        ToolTip(self.name_Entry,"Описание сервера, отображаемое при подключении в списке серверов. Поддерживает форматирование текста.")
+
+        port_Label = Label(self.root,text="Порт сервера:",font="arial 10")
+        port_Label.place(x=0,y=40)
+        ToolTip(port_Label,"Данный параметр определяет значение порта в протоколах TCP и UDP, который будет использовать сервер игры")
+        self.port_Entry = ttk.Entry(self.root,style="TEntry")
+        self.port_Entry.place(x=90,y=40,width=190,height=20)
+        ToolTip(self.port_Entry,"Данный параметр определяет значение порта в протоколах TCP и UDP, который будет использовать сервер игры")
+        
+        MP_Label = Label(self.root,text="Максимальное количество игроков:",font="arial 10")
+        MP_Label.place(x=0,y=80)
+        self.MP_Entry = ttk.Entry(self.root,style="TEntry")
+        self.MP_Entry.place(x=190,y=80,width=190,height=20)
+
+        onlineMode_Check = ttk.Checkbutton(self.root,text="Онлайн режим",variable=self.onlineMode_Choose,onvalue=1,offvalue=0)
+        onlineMode_Check.place(x=0,y=120)
+        ToolTip(onlineMode_Check, "Этот параметр позволяет включить/выключить проверку подлинности премиум-аккаунтов пользователей, которые подключаются к данному серверу.")
+
+        CBE_Check = ttk.Checkbutton(self.root,text="Командный блок",variable=self.CBE_Choose,onvalue=1,offvalue=0)
+        CBE_Check.place(x=0,y=160)
+        ToolTip(CBE_Check, "Позволяет использовать командный блок. Эта опция не генерируется при первой загрузке, а появляется при первой попытке использования командного блока.")
+
+        PVP_Check = ttk.Checkbutton(self.root,text="PVP",variable=self.PVP_Choose,onvalue=1,offvalue=0)
+        PVP_Check.place(x=0,y=200)
+        ToolTip(PVP_Check,"Включает/отключает получение урона игрокам от атак других игроков на сервере. При true игроки смогут «воевать» между собой, убивая друг друга. Если уставлено значение false, игроки не смогут наносить прямой урон один другому.")
+        
+        D_Label = Label(self.root,text="Сложность:",font="arial 10")
+        D_Label.place(x=0,y=240)
+        ToolTip(D_Label,"Уровень сложности")
+        self.D_Combo = ttk.Combobox(self.root,values = ["Мирная","Лёгкая","Нормальная","Сложная"],height=4,state="readonly")
+        self.D_Combo.place(x=72,y=240)
+        self.D_Combo.set("Нормальная")
+        self.D_Combo.bind("<FocusIn>", self.antiSelection)
+        ToolTip(self.D_Combo,"Уровень сложности")
+
+        GM_Label = Label(self.root,text="Режим игры:",font="arial 10")
+        GM_Label.place(x=0,y=280)
+        ToolTip(GM_Label,"Игровой режим по умолчанию")
+        self.GM_Combo = ttk.Combobox(self.root,values = ["Выживание","Креатив","Приключение","Наблюдатель"],height=4,state="readonly")
+        self.GM_Combo.place(x=80,y=280)
+        self.GM_Combo.set("Выживание")
+        self.GM_Combo.bind("<FocusIn>", self.antiSelection)
+        ToolTip(self.GM_Combo,"Игровой режим по умолчанию")
+
+        nextButton = ttk.Button(self.root,text="далее",style="TButton",command=self.nextAction) #Создание кнопки со свойствами
+        nextButton.place(x=485,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+
+        backButton = ttk.Button(self.root,text="назад",style="TButton",command=self.backAction, state=DISABLED) #Создание кнопки со свойствами
+        backButton.place(x=370,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+
+        if name is not None:
+            self.name_Entry.insert(1.0,name)
+
+        if port is not None:
+            self.port_Entry.insert(1.0,port)
+
+        if onlineMode is not None:
+            self.onlineMode_Choose.set(onlineMode)
+
+        if CBE is not None:
+            self.CBE_Choose.set(CBE)
+
+        if PVP is not None:
+            self.PVP_Choose.set(PVP)
+
+        if D is not None:
+            self.D_Combo.set(D)
+
+        if GM is not None:
+            self.GM_Choose.set(GM)
+
+        self.root.mainloop()
+        return self.returnArray
+
+    def antiSelection(self,event):
+        event.widget.master.focus_set()
+        pass
+    
+    def nextAction(self):
+        self.returnArray = ["NEXT",self.name_Entry.get(),self.port_Entry.get(),self.onlineMode_Choose.get(),self.CBE_Choose.get(),self.PVP_Choose.get(),self.MP_Entry.get(),self.D_Combo.get(),self.GM_Combo.get()]
+        self.root.destroy() #Уничтожение окна
+        pass
+    
+    def backAction(self):
+        self.returnArray = ["BACK",self.name_Entry.get(),self.port_Entry.get(),self.D_Combo.get(),self.GM_Combo.get(),self.CBE_Choose.get(),self.PVP_Choose.get()]
+        self.root.destroy() #Уничтожение окна
+        pass
+
+    def appExit(self):
+        if messagebox.askokcancel("Выйти?","Вы точно хотите выйти из программы?"): #Если результат ответа от пользователя положительный
+            quit() #то выполняется зарезервированная функция quit
+            pass
+        pass
+
+class showVersionWindow():
+    def show(self,VersionArray,Version):
+        self.returnArray = [] #Создание массива returnArray
+        self.root = Tk()
+        windowX = int(self.root.winfo_screenwidth() / 2 - 325) #Вычисление центра экрана пользователя по X'у
+        windowY = int(self.root.winfo_screenheight() / 2 - 200) #Вычисление центра экрана пользователя по Y'у
+        self.root.title("Reserv") #Сэтим имя окна 
+        self.root.geometry("650x400+" + str(windowX) + "+" + str(windowY)) #Сэтим размер окна
+        self.root.resizable(False, False) #Установка возможности изменить размер окна на False (По ширине и длине)
+        self.root.protocol("WM_DELETE_WINDOW", self.appExit) #Установка функции appExit на кнопку "X"
+
+        s = ttk.Style()
+        s.configure("TButton",bg="white",fg="black",font="arial 10") #Создание стиля "TButton"
 
         informationLabel = Label(text="Выберите версию сервера:",font="arial 10") #Создание текста со свойствами
         informationLabel.place(x=0,y=0) #Размещение текста по кординатам и установка площади
@@ -121,25 +200,28 @@ class showVersionWindow():
         canvas.place(x=0,y=45,height=300) #Размещение холста по кординатам и установка площади
         versionScroll.place(x=635,y=0,width=15,height=400) #Размещение scrollbar'а по кординатам и установка площади 
 
-        button_next = ttk.Button(self.root,text="далее",style="TButton",command=self.nextAction) #Создание кнопки со свойствами
-        button_next.place(x=485,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+        nextButton = ttk.Button(self.root,text="далее",style="TButton",command=self.nextAction) #Создание кнопки со свойствами
+        nextButton.place(x=485,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
 
-        button_back = ttk.Button(self.root,text="назад",style="TButton",command=self.backAction) #Создание кнопки со свойствами
-        button_back.place(x=370,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+        backButton = ttk.Button(self.root,text="назад",style="TButton",command=self.backAction,state=DISABLED) #Создание кнопки со свойствами
+        backButton.place(x=370,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+
+        if Version is not None:
+            self.serverChoose.set(Version)
 
         self.root.mainloop()
         return self.returnArray
     
     def nextAction(self):
-        self.root.destroy() #Уничтожение окна
         self.returnArray.insert(0,"NEXT") #Установка значения NEXT в 0 индекс массива
         self.returnArray.insert(1,self.serverChoose.get()) #Установка значения переменной serverChoose в 1 индекс массива через функцию get()
+        self.root.destroy() #Уничтожение окна
         pass
     
     def backAction(self):
-        self.root.destroy() #Уничтожение окна
         self.returnArray.insert(0,"BACK") #Установка значения BACK в 0 индекс массива
         self.returnArray.insert(1,self.serverChoose.get()) #Установка значения переменной serverChoose в 1 индекс массива через функцию get()
+        self.root.destroy() #Уничтожение окна
         pass
     
     def appExit(self):
@@ -148,10 +230,8 @@ class showVersionWindow():
             pass
         pass
 
-#НЕДОДЕЛАНО, НЕ ИСПОЛЬЗОВАТЬ!!!!
-
-class showSettingsWindow():
-    def show(self):
+class showServerInfoWindow():
+    def show(self,name,port,onlineMode,CBE,PVP,D,GM):
         self.returnArray = [] #Создание массива returnArray
         self.root = Tk()
         windowX = int(self.root.winfo_screenwidth() / 2 - 325) #Вычисление центра экрана пользователя по X'у
@@ -161,3 +241,106 @@ class showSettingsWindow():
         self.root.resizable(False, False) #Установка возможности изменить размер окна на False (По ширине и длине)
         self.root.protocol("WM_DELETE_WINDOW", self.appExit) #Установка функции appExit на кнопку "X"
 
+        s = ttk.Style()
+        s.configure("TButton",bg="white",fg="black",font="arial 10") #Создание стиля "TButton"
+        s.configure("TEntry",font="arial 10") #Создание стиля "TEntry"
+
+        serverNameLabel = Label(self.root,text="Имя сервера: " + name)
+        serverNameLabel.place(x=0,y=0)
+
+        portNameLabel = Label(self.root,text="Порт сервера: " + port)
+        portNameLabel.place(x=0,y=25)
+        
+        onlineModeLabel = Label(self.root,text="Онлайн режим: " + onlineMode)
+        onlineModeLabel.place(x=0,y=50)
+
+        CBELabel = Label(self.root,text="Командный блок: " + CBE)
+        CBELabel.place(x=0,y=75)
+
+        PVPLabel = Label(self.root,text="PVP: " + PVP)
+        PVPLabel.place(x=0,y=100)
+
+        DLabel = Label(self.root,text="Сложность: " + D)
+        DLabel.place(x=0,y=125)
+
+        GMLabel = Label(self.root,text="Игровой режим: " + GM)
+        GMLabel.place(x=0,y=150)
+
+        nextButton = ttk.Button(self.root,text="собрать!",style="TButton",command=self.nextAction) #Создание кнопки со свойствами
+        nextButton.place(x=485,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+
+        backButton = ttk.Button(self.root,text="отменить",style="TButton",command=self.backAction) #Создание кнопки со свойствами
+        backButton.place(x=370,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+        
+        self.root.mainloop()
+        return 
+        
+    def nextAction(self):
+        self.root.destroy()
+
+    def backAction(self):
+        quit(0)
+
+    def appExit(self):
+        if messagebox.askokcancel("Выйти?","Вы точно хотите выйти из программы?"): #Если результат ответа от пользователя положительный
+            quit() #то выполняется зарезервированная функция quit
+
+class showCreateWindow():
+    def show(self, event):
+        self.returnArray = [] #Создание массива returnArray
+        self.root = Tk()
+        windowX = int(self.root.winfo_screenwidth() / 2 - 325) #Вычисление центра экрана пользователя по X'у
+        windowY = int(self.root.winfo_screenheight() / 2 - 200) #Вычисление центра экрана пользователя по Y'у
+        self.root.title("Reserv") #Сэтим имя окна 
+        self.root.geometry("650x400+" + str(windowX) + "+" + str(windowY)) #Сэтим размер окна
+        self.root.resizable(False, False) #Установка возможности изменить размер окна на False (По ширине и длине)
+        self.root.protocol("WM_DELETE_WINDOW", self.ingore) #Установка функции appExit на кнопку "X"
+
+        self.infoCreateLabel = Label(self.root,text="")
+        self.infoCreateLabel.place(x=200,y=150)
+
+        event.set()
+        self.root.mainloop()
+
+    def exit():
+        self.root.destroy()
+
+    def ingore(self):
+        pass
+
+    def changeText(self,text):
+        self.infoCreateLabel.delete('1.0', END)
+        self.infoCreateLabel.insert(1.0,text)
+
+class showEndWindow():
+    def show(self):
+        self.returnArray = [] #Создание массива returnArray
+        self.root = Tk()
+        windowX = int(self.root.winfo_screenwidth() / 2 - 325) #Вычисление центра экрана пользователя по X'у
+        windowY = int(self.root.winfo_screenheight() / 2 - 200) #Вычисление центра экрана пользователя по Y'у
+        self.root.title("Reserv") #Сэтим имя окна 
+        self.root.geometry("650x400+" + str(windowX) + "+" + str(windowY)) #Сэтим размер окна
+        self.root.resizable(False, False) #Установка возможности изменить размер окна на False (По ширине и длине)
+        #self.root.protocol("WM_DELETE_WINDOW", quit) #Установка функции appExit на кнопку "X"
+
+        s = ttk.Style()
+        s.configure("TButton",bg="white",fg="black",font="arial 10") #Создание стиля "TButton"
+        s.configure("TEntry",font="arial 10") #Создание стиля "TEntry"
+
+        infoLabel = Label(self.root,text="Сервер готов!",font="arial 10") #Создание текста со свойствами
+        infoLabel.place(x=200,y=150,width=250,height=50) #Размещение текста по кординатам и установка площади
+
+        nextButton = ttk.Button(self.root,text="старт!",style="TButton",command=self.nextAction) #Создание кнопки со свойствами
+        nextButton.place(x=485,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+
+        backButton = ttk.Button(self.root,text="выйти",style="TButton",command=self.backAction) #Создание кнопки со свойствами
+        backButton.place(x=370,y=370,width=100,height=25) #Размещение кнопки по кординатам и установка площади
+
+        self.root.mainloop()
+    def nextAction(self):
+        self.returnArray = ["NEXT"]
+        self.root.destroy()
+
+    def backAction(self):
+        self.returnArray = ["BACK"]
+        self.root.destroy()
--- startgui.py
+++ startgui.py
@@ -1,13 +1,178 @@
-print("Запуск Reserv с интегрированым интерфейсом.")
-import gui
+'''
+Reserv Lite
 
-objWelcomeWindow = gui.showWelcomeWindow()
-objWelcomeWindow.show()
+Copyright © Upbits Team, 2017 – 2018.
+GitHub: http://github.com/Upbits/
 
-objPathWindow = gui.showPathWindow()
-objPathWindow.show()
+По вопросом Reserv писать сюда:
+https://vk.com/reserv или на почту
+<reserv@blinkhub.ru>.
 
-VersionArray = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4]
+Удачного использования :D
+'''
+try:
+    import os, zipfile, wget, essentials, platform, time, requests, gui, belfrywidgets, threading #импорт всех нужных библиотек
+except:
+    import platform, os
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+    creatingWindow.changeText("У вас не найдены некоторые библиотеки которые нужны для работы программы, либо при импорте библиотек произошла ошибка. Сейчас произайдет автоматическая установка нужных библиотек.")
+    if platform.system() == "Windows":
+        os.system("pip3 install wget requests")
+    else:
+        os.system("sudo pip3 install wget requests")
+    creatingWindow.changeText("Все библиотеки установлены. Сейчас произайдет перезапуск Reserv.")
+    time.sleep(3)
+    os.system("python3 lite.py")
 
-objVersionWindow = gui.showVersionWindow()
-print(objVersionWindow.show(VersionArray))
+ #вызов функции универсальной отчистки
+welcome = gui.showWelcomeWindow()
+welcome.show()
+
+
+#приветствие и ввод адреса сервера
+config = gui.showSettingsWindow()
+s = config.show(None, None, None, None, None, None, None)
+serverName = s[1]
+serverPort = s[2]
+
+if serverName == "":
+    serverName = "Reserv Server"
+
+if serverPort == "":
+    serverPort = "25565"
+
+#диалоговое окно в терминале кароч
+#выбор online-mode на true или false
+serverMode = s[3]
+
+#максимальное колличество игроков
+serverMaxPlayers = s[6]
+if serverMaxPlayers == "":
+    serverMaxPlayers = "20"
+
+#доступны ли командные блоки на сервере
+serverCBE = s[4]
+
+#доступно ли PVP
+serverPVP = s[5]
+
+serverDiff = s[7]
+
+gameMode = s[8]
+
+#ядро сервера
+ver = gui.showVersionWindow()
+verList = ["1.12", "1.11", "1.10", "1.9", "1.8", "1.5"]
+choosedVersion = ver.show(verList, None)
+serverCore = str(verList[int(choosedVersion[1]) - 1])
+
+#начинается полный пипец
+info = gui.showServerInfoWindow()
+if serverMode == 1:
+    serverMode = "да"
+else:
+    serverMode = "нет"
+if serverCBE == 1:
+    serverCBE = "да"
+else:
+    serverCBE = "нет"
+if serverPVP == 1:
+    serverPVP = "да"
+else:
+    serverPVP = "нет"
+
+info.show(serverName, serverPort, str(serverMode), str(serverCBE), str(serverPVP), serverDiff, gameMode)
+
+controlEvent = threading.Event()
+creatingWindow = gui.showCreateWindow()
+
+def windowThread():
+    creatingWindow.show(controlEvent)
+
+t = threading.Thread(target=windowThread)
+t.start()
+
+controlEvent.wait()
+
+creatingWindow.changeText("Загрузка сервера...")
+#скачивается база сервера с сервера
+#прямо рекурсия получается
+wget.download("http://hack.blinkhub.ru/reserv/guiserv.zip", "server.zip")
+creatingWindow.changeText("Распаковка сервера...")
+#распаковывается сервер
+#используя функцию unZip из
+#нашей библиотеки Essentials
+essentials.unZip("server.zip", "server/")
+os.remove("server.zip")
+creatingWindow.changeText("Загрузка ядра....")
+#дальше идет загрузка самого важного компанента
+#сервера – ядра... короче просто подставляется 
+#переменная с версией ядра... все очень просто
+#и с костылями конечго-же... потом как-нибуть...
+wget.download("http://hack.blinkhub.ru/reserv/cores/" + serverCore + ".jar", "server/")
+creatingWindow.changeText("\nНастройка параметров...")
+#уу! настройка параметров!
+#готовится самая главная переменная
+#в которую записан весь текст из файла
+#конфигурации сервера Minecraft
+servProperties = essentials.textFromFile("server/server.properties") #вот она это строчка судьбы
+#дальше идет намализация переменных
+#которче, готовится к тому что-бы быть
+#записанным в файл конфига
+if serverDiff == "Мирная":
+    serverDiff = "0"
+elif serverDiff == "Легкая":
+    serverDiff = "1"
+elif serverDiff == "Нормальная":
+    serverDiff = "2"
+elif serverDiff == "Сложная":
+    serverDiff = "3"
+
+if gameMode == "Выживание":
+    gameMode = "0"
+elif gameMode == "Креатив":
+    gameMode = "1"
+elif gameMode == "Приключение":
+    gameMode = "2"
+elif gameMode == "Наблюдатель":
+    gameMode = "3"
+
+if serverMode == "да":
+    serverMode = "true"
+else:
+    serverMode = "false"
+
+if serverCBE == "да":
+    serverCBE = "true"
+else:
+    serverCBE = "false"
+
+if serverPVP == "да":
+    serverPVP = "true"
+else:
+    serverPVP = "false"
+#дальше самое интересное!
+#идет вызов функции записи данных в файл
+#и для записи передается огромная переменная
+#которая тут-же и изменяется.
+#в той базе сервера заготовлен конфиг
+#с уже заготовленными параметрами, а
+#эта программа просто заменяет те значения на
+#другие, который ввел пользователь
+superDuperCustomConfig = servProperties.replace("server-port=RESERV", "server-port=" + serverPort).replace("online-mode=RESERV", "online-mode=" + serverMode).replace("motd=RESERV", "motd=" + serverName).replace("pvp=RESERV", "pvp=" + serverPVP).replace("max-players=RESERV", "max-players=" + serverMaxPlayers).replace("enable-command-block=RESERV", "enable-command-block=" + serverCBE).replace("gamemode=RESERV", "gamemode=" + gameMode).replace("difficulty=RESERV", "difficulty=" + serverDiff)
+essentials.textToFile("server/server.properties", superDuperCustomConfig)
+#мне лень дальше комментировать все это
+#надеюсь что ты не глупый человек и сам
+#сможешь в этом разрбраться...
+if platform.system() == "Windows":
+    essentials.textToFile("server/start.bat", "@echo Reserv-Server\njava -Xmx 1024M -Xms1024M -jar " + serverCore + ".jar nogui\n@PAUSE")
+else:
+    essentials.textToFile("server/start.sh", "echo \"Reserv-Server\"\njava -Xmx1024M -Xms1024M -jar " + serverCore + ".jar nogui")
+creatingWindow.exit()
+
+
+allDone = gui.showEndWindow()
+allDone.show()
